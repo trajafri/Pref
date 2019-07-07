676d86e856571774c2ceefea0d10a200e0dbb599
Turab Jafri <trajafri@gmail.com>
2019-07-07 17:35:33 -0500

    Use Parsec for lexer

--- src/Lexer.hs
+++ src/Lexer.hs
@@ -1,14 +1,60 @@
 {-# LANGUAGE OverloadedStrings, ViewPatterns  #-}
 
 module Lexer
-  ( tokenize
+  ( decimal
+  , identifier
+  , parens
+  , stringLiteral
+  , tokenize
+  , whiteSpace
   )
 where
 
+import           Control.Monad.Identity
 import           Data.Char
 import qualified Data.Text                     as T
 import           Errors
 import           Syntax.Tokens
+import           Text.Parsec
+import           Text.ParserCombinators.Parsec.Language
+import qualified Text.ParserCombinators.Parsec.Token
+                                               as PT
+
+{- Regarding using the "user state" in ParsecT:
+   If we were to stack ParsecT on some monad M, the
+   difference would be that ParsecT's state
+   (aka "user state") also backtracks on failure, but
+   the effects of M do not!
+   Imagine backtracking after performing IO. The IO
+   effect would should up even if ParsecT backtracked.
+-}
+
+prefDefinition :: LanguageDef ()
+prefDefinition = emptyDef { commentLine   = ";"
+                          , commentStart  = "|#"
+                          , commentEnd    = "|#"
+                          , caseSensitive = True
+                          }
+
+lexer :: PT.GenTokenParser String () Identity
+lexer = PT.makeTokenParser prefDefinition
+
+identifier :: ParsecT String () Identity String
+identifier = do
+  idName@(idC : ids) <- many1 (alphaNum <|> satisfy validIdChar)
+  if isDigit idC && all isDigit ids then mzero else return idName
+
+parens :: ParsecT String () Identity String -> ParsecT String () Identity String
+parens = PT.parens lexer
+
+stringLiteral :: ParsecT String () Identity String
+stringLiteral = PT.stringLiteral lexer
+
+decimal :: ParsecT String () Identity Integer
+decimal = PT.decimal lexer
+
+whiteSpace :: ParsecT String () Identity ()
+whiteSpace = PT.whiteSpace lexer
 
 tokenize :: T.Text -> Either Error [Token]
 tokenize = lexer
@@ -45,4 +91,23 @@ tokenize = lexer
 
 
 validIdChar :: Char -> Bool
-validIdChar c = all ($ c) [not . isSpace, (')' /=), ('(' /=), ('\"' /=)]
+validIdChar c = all
+  ($ c)
+  [ not . isSpace
+  , (')' /=)
+  , ('(' /=)
+  , ('\"' /=)
+  , ('\'' /=)
+  , ('.' /=)
+  , (',' /=)
+  , ('[' /=)
+  , (']' /=)
+  , ('{' /=)
+  , ('}' /=)
+  , ('<' /=)
+  , ('>' /=)
+  , ('#' /=)
+  , ('`' /=)
+  , (':' /=)
+  , (';' /=)
+  ]
