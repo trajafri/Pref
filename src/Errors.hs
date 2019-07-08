fe61ba81a275b0540349e73b70c720a5bd92bda1
Turab Jafri <trajafri@gmail.com>
2019-07-07 21:14:11 -0500

    Further Parsec changes

--- src/Errors.hs
+++ src/Errors.hs
@@ -1,17 +1,8 @@
 module Errors
-  ( Error(..)
+  ( EvalError(..)
   )
 where
 
 import qualified Data.Text                     as T
 
--- Warning: This is a really bad way to handle errors.
--- My approach is is almost as bad as the type 'AnyError'
-data Error
-  = ParseError T.Text
-  | EvalError T.Text
-  | LexerError T.Text
-  deriving (Show, Eq)
-
--- I was thinking of making an Error type class, but that seems
--- useless
+data EvalError = EvalError T.Text deriving (Show, Eq)
