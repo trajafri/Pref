9a4f47f93ef2a68ee073f37e8bbc078661021809
Turab Jafri <trajafri@gmail.com>
2019-07-27 23:08:01 -0500

    Replace nest with hang in prettier

--- src/Syntax/Exp.hs
+++ src/Syntax/Exp.hs
@@ -33,23 +33,22 @@ instance Pretty Exp where
   pretty (SLiteral s) = pretty s
   pretty (Lambda v b) =
     let varDoc = parens . hsep $ pretty <$> v
-    in  parens
-          $ vcat [pretty "lambda" <+> varDoc, nest (pred indentC) $ pretty b]
+    in  parens $ hang (pred indentC) $ vcat
+          [pretty "lambda" <+> varDoc, pretty b]
   pretty (If cnd thn els) =
     let cndDoc = pretty cnd
         thnDoc = pretty thn
         elsDoc = pretty els
-    in  parens $ (pretty "if" <+> (vcat [cndDoc, thnDoc, elsDoc]))
+    in  parens $ hang indentC $ vcat [pretty "if" <+> cndDoc, thnDoc, elsDoc]
   pretty (Let bindings b) =
     let (vars, binds) = unzip bindings
         bindDoc       = vcat $ map parens $ zipWith (<+>)
                                                     (map pretty vars)
                                                     (map pretty binds)
-    in  parens $ vcat [pretty "let" <+> bindDoc, nest (pred indentC) $ pretty b]
-  pretty (App rator rands) =
-    parens $ (foldr1 (<+>) $ map pretty (rator : rands))
-  pretty (Def v bind) = parens
-    $ vcat [pretty "define" <+> pretty v, nest (pred indentC) $ pretty bind]
+    in  parens $ hang (pred indentC) $ vcat [pretty "let" <+> bindDoc, pretty b]
+  pretty (App rator rands) = parens (foldr1 (<+>) $ map pretty (rator : rands))
+  pretty (Def v     bind ) = parens $ hang (pred indentC) $ vcat
+    [pretty "define" <+> pretty v, pretty bind]
 
 instance Show Exp where
   show = show . pretty
