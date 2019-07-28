d3412c65ae2a463d98ee26586e3c82f89b4aacb0
Turab Jafri <trajafri@gmail.com>
2019-07-27 22:23:22 -0500

    Simple cps exe

---
+++ src/CpsApp.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE TupleSections #-}
+
+module Main where
+
+import qualified Data.Text                     as T
+import qualified Pref                          as P
+import           System.Console.ArgParser
+import           Transform.CPS
+
+cliParser :: ParserSpec (String, String)
+cliParser = (,) `parsedBy` reqPos "filePath" `andBy` reqPos "outFilePath"
+
+cps :: (String, String) -> IO ()
+cps (fp, op) = do
+  fileContent <- readFile fp
+  ast <- either (fail "Provided file should be syntatically correct.") return
+    $ P.codeToAst (T.pack fileContent)
+  writeFile op $ unwords $ map (show . cpser) ast
+
+main :: IO ()
+main = withParseResult cliParser cps
