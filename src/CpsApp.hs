4bb32657ec332054541e176a5ea1fc6585a87348
Turab Jafri <trajafri@gmail.com>
2019-08-04 20:03:54 -0500

    Add changes to `cps` to support two modes

--- src/CpsApp.hs
+++ src/CpsApp.hs
@@ -1,20 +1,59 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import           Control.Monad.State
 import           Data.List
 import qualified Data.Text                     as T
 import qualified Pref                          as P
 import           System.Console.ArgParser
+import           Syntax.Exp
 import           Transform.CPS
 
-cliParser :: ParserSpec (String, String)
-cliParser = (,) `parsedBy` reqPos "filePath" `andBy` reqPos "outFilePath"
+data FreeAndScoped = FAS [(T.Text, Int)] [T.Text]
+instance Collector FreeAndScoped where
+  collect e@(var, arity) c@(FAS free scoped) =
+    if elem e free then c else FAS ((var <> "k", arity) : free) scoped
 
-cps :: (String, String) -> IO ()
-cps (fp, op) = do
+  getFixedExp i@(Id txt) (FAS free _) =
+    if elem txt $ map fst free then Id $ txt <> "k" else i
+  getFixedExp ex _ = ex
+
+  getFreeVars (FAS free _) = free
+
+  updateVars newVars (FAS free oldVars) = FAS free $ newVars <> oldVars
+
+  removeVars oldVars (FAS free newVars) = FAS free $ newVars \\ oldVars
+
+cliParser :: ParserSpec (String, String, Bool)
+cliParser =
+  (,,)
+    `parsedBy` reqPos "filePath"
+    `andBy`    reqPos "outFilePath"
+    `andBy`    boolFlag "Add free variable definitions"
+
+cps :: (String, String, Bool) -> IO ()
+cps (fp, op, defineFree) = do
+  let collector = FAS [] []
   fileContent <- readFile fp
   ast <- either (fail "Provided file should be syntatically correct.") return
     $ P.codeToAst (T.pack fileContent)
-  writeFile op $ intercalate "\n" $ map (show . cpser) ast
+  let (cpsedExps, FAS free _) = flip runState collector $ mapM cpser ast
+  let cpsedFile               = intercalate "\n" $ map show cpsedExps
+  let definitions = if defineFree
+        then intercalate "\n" $ map (show . defineFreeVar) free <> ["\n"]
+        else ""
+  writeFile op $ definitions <> cpsedFile
+
+ where
+  defineFreeVar :: (T.Text, Int) -> Exp
+  defineFreeVar (var, arity) =
+    let vars = map (T.pack . show) [1 .. arity]
+    in  Def var $ Lambda (vars <> ["k"]) $ App
+          (Id "k")
+          [App (Id $ var <> "k") $ map Id vars]
+
 
 main :: IO ()
 main = withParseResult cliParser cps
+
