7566a51c41b22ad28fba38feffa9337b87172284
Turab Jafri <trajafri@gmail.com>
2019-08-12 15:08:42 -0500

    Use Collector sum type in CPS app

--- src/CpsApp.hs
+++ src/CpsApp.hs
@@ -10,21 +10,6 @@ import           System.Console.ArgParser
 import           Syntax.Exp
 import           Transform.CPS
 
-data FreeAndScoped = FAS [(T.Text, Int)] [T.Text]
-instance Collector FreeAndScoped where
-  collect e@(var, _) c@(FAS free scoped) =
-    if elem e free || elem var scoped then c else FAS (e : free) scoped
-
-  getFixedExp i@(Id txt) (FAS free _) =
-    if elem txt $ map fst free then Id $ txt <> "k" else i
-  getFixedExp ex _ = ex
-
-  getFreeVars (FAS free _) = free
-
-  updateVars newVars (FAS free oldVars) = FAS free $ newVars <> oldVars
-
-  removeVars oldVars (FAS free newVars) = FAS free $ newVars \\ oldVars
-
 cliParser :: ParserSpec (String, String, Bool)
 cliParser =
   (,,)
@@ -34,15 +19,16 @@ cliParser =
 
 cps :: (String, String, Bool) -> IO ()
 cps (fp, op, defineFree) = do
-  let collector = FAS [] []
+  let collector = if defineFree then FreeAndScoped [] [] else Unit
   fileContent <- readFile fp
   ast <- either (fail "Provided file should be syntatically correct.") return
     $ P.codeToAst (T.pack fileContent)
-  let (cpsedExps, FAS free _) = flip runState collector $ mapM cpser ast
+  let (cpsedExps, collection) = flip runState collector $ mapM cpser ast
   let cpsedFile               = intercalate "\n" $ map show cpsedExps
-  let definitions = if defineFree
-        then intercalate "\n" $ map (show . defineFreeVar) free <> ["\n"]
-        else ""
+  let definitions =
+        intercalate "\n"
+          $  map (show . defineFreeVar) (getFreeVars collection)
+          <> ["\n"]
   writeFile op $ definitions <> cpsedFile
 
  where
